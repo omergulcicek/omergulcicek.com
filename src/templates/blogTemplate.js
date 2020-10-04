36a3fda2ff68298ca84234a5cdb8ee4d74f85f8a
Ömer Gülçiçek <iletisim@omergulcicek.com>
2020-10-04 22:19:46 +0300

    Bozulan bir karakter svg olarak eklendi.

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -20,7 +20,11 @@ export default function Template({ data }) {
 
       <section>
         <Details>
-          <Link to="/blog/">{"<- Tüm Yazılar"}</Link>
+          <Link to="/blog/">
+            <svg aria-hidden="true" role="img" viewBox="0 0 448 512" height="14px"><path fill="currentColor" d="M257.5 445.1l-22.2 22.2c-9.4 9.4-24.6 9.4-33.9 0L7 273c-9.4-9.4-9.4-24.6 0-33.9L201.4 44.7c9.4-9.4 24.6-9.4 33.9 0l22.2 22.2c9.5 9.5 9.3 25-.4 34.3L136.6 216H424c13.3 0 24 10.7 24 24v32c0 13.3-10.7 24-24 24H136.6l120.5 114.8c9.8 9.3 10 24.8.4 34.3z"></path></svg>
+
+            <span>Tüm Yazılar</span>
+          </Link>
           
           <span>•</span>
           <span>{ date }</span>
@@ -78,6 +82,10 @@ const Details = styled.div`
   font-size: 14px;
   line-height: 18px;
   margin-top: 40px;
+
+  svg {
+    vertical-align: middle;
+  }
 
   a {
     text-decoration: none;
