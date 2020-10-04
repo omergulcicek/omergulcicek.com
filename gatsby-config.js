e7b7e5be51ee3814a60043bce675a453409a8ea4
Ömer Gülçiçek <iletisim@omergulcicek.com>
2020-10-04 22:19:38 +0300

    Yazı stili "rubik" yapıldı.

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,7 +44,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
-        fonts: [`limelight`, `inter\:300,400,700,900`],
+        fonts: [`limelight`, `Rubik\:300,400,700,900`],
         display: "swap",
       },
     },
