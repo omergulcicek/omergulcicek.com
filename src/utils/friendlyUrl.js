856f2ea6460a45ccec1bd50f36570aee4bf59979
omergulcicek <iletisim@omergulcicek.com>
2020-12-31 21:23:06 +0300

    friendlyUrl klasöre taşındı

---
+++ src/utils/friendlyUrl.js
@@ -0,0 +1,13 @@
+export default function friendlyUrl(url) {
+  return url.toString()
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g,'')
+    .replace(/\s+/g,'-')
+    .replace(/\ı+/g,'i')
+    .toLowerCase()
+    .replace(/&/g,'-and-')
+    .replace(/[^a-z0-9\-]/g,'')
+    .replace(/-+/g,'-')
+    .replace(/^-*/,'')
+    .replace(/-*$/,'');
+}
