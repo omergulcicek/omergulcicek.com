d7520b2e61101b294da2241a6f051ed7d535d12a
omergulcicek <iletisim@omergulcicek.com>
2021-03-06 15:26:58 +0300

    bilgisayar koltuğu eklendi

--- src/assets/desk.js
+++ src/assets/desk.js
@@ -7,27 +7,21 @@ export default function MyDesk (){
       "done": true
     },
     {
-      "img": "magic-trackpad-2",
-      "title": "Magic Trackpad 2",
-      "desc": "Kablosuz Mouse",
-      "done": false
-    },
-    {
       "img": "logitech-g513",
       "title": "Logitech G513 Carbon",
       "desc": "Bilgisayar Klavyesi",
       "done": true
     },
     {
+      "img": "magic-trackpad-2",
+      "title": "Magic Trackpad 2",
+      "desc": "Kablosuz Mouse",
+      "done": false
+    },
+    {
       "img": "samsung-space",
       "title": "Samsung Space",
       "desc": "32\" 4K Monitör",
-      "done": true
-    },
-    {
-      "img": "logitech-g231",
-      "title": "Logitech G231",
-      "desc": "Kulaküstü Kulaklık",
       "done": true
     },
     {
@@ -37,21 +31,27 @@ export default function MyDesk (){
       "done": false
     },
     {
+      "img": "adore-koltuk",
+      "title": "Adore Manager Techno",
+      "desc": "Bilgisayar Koltuğu",
+      "done": true
+    },
+    {
       "img": "iphone-12-mini",
       "title": "Apple iPhone 12 Mini",
       "desc": "Cep Telefonu",
       "done": true
     },
     {
-      "img": "watch-series-6",
-      "title": "Apple Watch Series 6",
-      "desc": "Akıllı Saat",
-      "done": false
-    },
-    {
       "img": "airpods-pro",
       "title": "Airpods Pro",
       "desc": "Kablosuz Kulaklık",
+      "done": true
+    },
+    {
+      "img": "logitech-g231",
+      "title": "Logitech G231",
+      "desc": "Kulaküstü Kulaklık",
       "done": true
     },
   ]
