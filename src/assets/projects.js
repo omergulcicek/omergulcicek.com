2fe7024d4125ed185ac7f6cdcdeb418064d541c6
Ömer Gülçiçek <iletisim@omergulcicek.com>
2021-06-08 00:26:09 +0300

    Yeni proje eklendi

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -5,7 +5,7 @@ export default function Projects (){
       "img": "https://omergulcicek.com/img/turkuazcss.png",
       "title": "Turkuaz",
       "subtitle": "Hızlı esnek ve kolay web sayfaları geliştirmek için tasarlanan güçlü bir CSS Framework",
-      "desc": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure recusandae distinctio nostrum assumenda ea debitis consectetur sint quam aliquam minus a, nulla facere. Commodi quisquam a culpa cupiditate eos pariatur!",
+      "desc": "Yalnızca CSS içeren ve özelleştirilebilir bileşenlerden oluşan bir kütüphanesi. SCSS, React ve Vue kütüphane desteği bulunmakta. Açık kaynak olarak geliştirilmektedir.",
       "tags": ["css", "scss", "framework", "react", "vue"]
     },
     {
@@ -13,7 +13,7 @@ export default function Projects (){
       "img": "https://omergulcicek.com/img/turkcedokuman.png",
       "title": "Türkçe Doküman",
       "subtitle": "Yazılım alanında sıfırdan eğitim veren Türkçe dokümanlar",
-      "desc": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure recusandae distinctio nostrum assumenda ea debitis consectetur sint quam aliquam minus a, nulla facere. Commodi quisquam a culpa cupiditate eos pariatur!",
+      "desc": "CSS, SCSS, JavaScript, ES6, React, SPA, Pyhton, Shell, Linux, Rust gibi teknik çeviriler ve kod yazım stili rehberlerine kadar bir çok Türkçe dokümanın toplandığı bir proje. Açık kaynak olarak geliştirilmektedir, sizlerde doküman ekleyebilirsiniz.",
     },
     {
       "link": "https://github.com/omergulcicek/bootcamp",
@@ -21,7 +21,7 @@ export default function Projects (){
       "title": "Frontend Bootcamp",
       "subtitle": "Kodluyoruz.org çatısı altında ilerletilen Frontend eğitiminin ders notları",
       "tags": ["html", "css", "scss", "javascript", "es6", "react", "spa"],
-      "desc": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure recusandae distinctio nostrum assumenda ea debitis consectetur sint quam aliquam minus a, nulla facere. Commodi quisquam a culpa cupiditate eos pariatur!",
+      "desc": "HTML seviyesinden React ile Single Page Application yazacak seviyeye kadar 30'a yakın öğrenciye verdiğim eğitimin ders notları. Eğitim boyunca toplamda 3 tasarım koda döküldü. Frontend'e nereden başlamak istediğini bilmeyenler için bir yol haritası.",
     },
     {
       "link": "https://github.com/omergulcicek/react-page-progress-bar",
@@ -29,15 +29,15 @@ export default function Projects (){
       "title": "React Page Progress Bar",
       "subtitle": "Sayfadaki konumu dairesel bir progress bar'da gösterir ve tıklanıldığında üste çıkar",
       "tags": ["react", "npm", "library"],
-      "desc": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure recusandae distinctio nostrum assumenda ea debitis consectetur sint quam aliquam minus a, nulla facere. Commodi quisquam a culpa cupiditate eos pariatur!",
+      "desc": "Web sitelerinin sağındaki sıradan scroll'ları sevmeyenler için geliştirilmiş olan bir React componenti. Sayfada scroll yaptıkça yüzdelik barı dolacak; renk, boyut, otomatik gizleme modu gibi bir çok seçenek isteğe bağlı değiştirilebiliyor.",
     },
     {
       "link": "https://github.com/omergulcicek/omergulcicek.com",
       "img": "https://omergulcicek.com/img/omergulcicek.png",
       "title": "omergulcicek.com",
       "subtitle": "Kişisel sitemin kaynak kodları",
-      "tags": ["react", "gatsby", "graphql", "sytled-component"],
-      "desc": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure recusandae distinctio nostrum assumenda ea debitis consectetur sint quam aliquam minus a, nulla facere. Commodi quisquam a culpa cupiditate eos pariatur!",
+      "tags": ["react", "gatsby", "graphql", "styled-component"],
+      "desc": "Gatsby React başlangıç kiti ve Styled Component'ler ile geliştirilen kişisel web sitemin, önceki versiyonları da dahil olmak üzere yaptığım her geliştirmeyi GitHub'ta yayınlıyorum; kodları inceleyebilir ve kullanabilirsiniz.",
     },
     {
       "link": "https://github.com/omergulcicek/react",
@@ -46,7 +46,7 @@ export default function Projects (){
       "subtitle": "React Türkçe çevirisi",
       "button": "github.com/omergulcicek/react",
       "tags": ["react", "hooks"],
-      "desc": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure recusandae distinctio nostrum assumenda ea debitis consectetur sint quam aliquam minus a, nulla facere. Commodi quisquam a culpa cupiditate eos pariatur!",
+      "desc": "Facebook'un geliştirdiği JavaScript kütüphanesi olan React'ın dokümantasyonun Türkçe çevirisi. Bu doküman React resmi olarak Türkçe desteği almadan önce hazırlanmıştı; şimdi güncellenmiyor.",
     },
     {
       "link": "https://github.com/omergulcicek/es6",
@@ -54,7 +54,7 @@ export default function Projects (){
       "title": "ECMAScript 6",
       "subtitle": "ECMAScript 6 Türkçe çevirisi",
       "tags": ["javascript", "es6"],
-      "desc": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure recusandae distinctio nostrum assumenda ea debitis consectetur sint quam aliquam minus a, nulla facere. Commodi quisquam a culpa cupiditate eos pariatur!",
+      "desc": "EcmaScript'in yeni sürümleriyle birlikte JavaScript dünyasına getirdiği/getireceği yeniliklerin örneklerle anlatıldığı kitabın Türkçe çevirisi.",
     },
     {
       "link": "https://github.com/omergulcicek/article-time-ago",
@@ -62,7 +62,15 @@ export default function Projects (){
       "title": "Article Time Ago",
       "subtitle": "Belirtilen tarih ile anlık tarih arasındaki süreyi sözel olarak yazar",
       "tags": ["javascript", "npm", "library"],
-      "desc": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure recusandae distinctio nostrum assumenda ea debitis consectetur sint quam aliquam minus a, nulla facere. Commodi quisquam a culpa cupiditate eos pariatur!",
+      "desc": "Tarih formatındaki değeri sözel olarak çeviren npm paketi. Parametre aldığı tarih yakın bir tarihse bugün, dün, evvelsi gün, 3/4/5/6 gün önce; biraz daha eski tarih ise geçen hafta/2 hafta önce vb; daha öncesi ise geçen ay/2 ay önce/geçen yıl/2 yıl önce vb formatında yazar.",
+    },
+    {
+      "link": "https://github.com/omergulcicek/frontend-boilerplate",
+      "img": "https://omergulcicek.com/img/css.png",
+      "title": "Frontend Boilerplate",
+      "subtitle": "Frontend projeleriniz için bir başlangıç kiti",
+      "tags": ["css", "scss", "javascript", "es6", "minify", "autoprefixer", "eslint"],
+      "desc": "Bir site geliştirirken lazım olan her şeyin paket haline getirilmiş hali. SCSS desteği ile kodlarınızı otomatik olarak CSS'e çevirip sıkıştırır. CSS'inizin diğer tarayıcılar ile uyumlu çalışmasını sağlar. ES6 ile yazdığınız kodları JavaScript'e çevirip sıkıştırır. Kod standartları sağlar.",
     }
   ]
 }
