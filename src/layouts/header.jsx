7a314cd1019decc34bb55332a67734a0f5eb1db5
Ömer Gülçiçek <iletisim@omergulcicek.com>
2023-12-16 02:20:27 +0300

    feat: added links to header

--- src/layouts/header.jsx
+++ src/layouts/header.jsx
@@ -1,19 +1,31 @@
 import Link from "next/link"
+import { useRouter } from "next/router"
 
+import { cn } from "lib/utils"
 import { navigationMenu } from "lib/constant"
 
 export default function Header() {
-  if (navigationMenu?.length === 0) return false
+  if (!navigationMenu || navigationMenu?.length === 0) return false
+
+  const { pathname } = useRouter()
 
   return (
     <>
-      <header className="w-full border-b z-40">
+      <header className="w-full border-b dark:border-b-neutral-800 z-40">
         <div className="container">
           <nav>
-            <ul className="flex items-center gap-4 h-16">
+            <ul className="flex items-center gap-4 h-16 px-4">
               {navigationMenu.map(({ text = "", link = "/" }, index) => (
                 <li key={index}>
-                  <Link href={link}>{text}</Link>
+                  <Link
+                    href={link}
+                    className={cn(
+                      "cursor-pointer transition hover:underline",
+                      pathname === link && "text-primary"
+                    )}
+                  >
+                    {text}
+                  </Link>
                 </li>
               ))}
             </ul>
