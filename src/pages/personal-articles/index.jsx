e46f8ab1516d77250b179e8703d5a5a6f552a805
Ömer Gülçiçek <iletisim@omergulcicek.com>
2023-12-19 01:52:18 +0300

    feat: new pages,

---
+++ src/pages/personal-articles/index.jsx
@@ -0,0 +1,9 @@
+export default function PersonalArticles() {
+  return (
+    <>
+      <div className="container">
+        <h1 className="text-3xl font-bold underline">Personal Articles</h1>
+      </div>
+    </>
+  )
+}
---
+++ src/pages/technical-articles/index.jsx
@@ -0,0 +1,9 @@
+export default function TechnicalArticles() {
+  return (
+    <>
+      <div className="container">
+        <h1 className="text-3xl font-bold underline">Technical Articles</h1>
+      </div>
+    </>
+  )
+}
