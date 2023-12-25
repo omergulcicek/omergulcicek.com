9585b95f2d05750c258ce1cc406d8a3047e91c2b
Ömer Gülçiçek <iletisim@omergulcicek.com>
2023-12-25 21:56:03 +0300

    feat: react tweets

---
+++ next.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  transpilePackages: ["react-tweet"],
+}
+
+module.exports = nextConfig
--- src/pages/tweets/index.jsx
+++ src/pages/tweets/index.jsx
@@ -1,8 +1,14 @@
-export default function Tools() {
+import { Tweet } from "react-tweet"
+
+export default function Tweets() {
   return (
     <>
       <div className="container">
         <h1 className="text-3xl font-bold underline">Tweets</h1>
+
+        <div>
+          <Tweet id="1717596665690091542" />
+        </div>
       </div>
     </>
   )
