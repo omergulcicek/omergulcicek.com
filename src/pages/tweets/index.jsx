8ae003d98a27ccffa20d0d4cdffaa0fe2a3b80ef
Ömer Gülçiçek <iletisim@omergulcicek.com>
2023-12-25 22:05:50 +0300

    feat: added animations to tweet components

--- src/pages/tweets/index.jsx
+++ src/pages/tweets/index.jsx
@@ -1,13 +1,22 @@
+import { motion } from "framer-motion"
 import { Tweet } from "react-tweet"
 
 export default function Tweets() {
   return (
     <>
-      <div className="container">
+      <div className="container px-4">
         <h1 className="text-3xl font-bold underline">Tweets</h1>
 
-        <div>
-          <Tweet id="1717596665690091542" />
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-12">
+          {new Array(6).fill({}).map((e, i) => (
+            <motion.div
+              initial={{ opacity: 0, translateY: "100px" }}
+              animate={{ opacity: 1, translateY: "0px" }}
+              transition={{ delay: (i + 1) / 10 }}
+            >
+              <Tweet id="1717596665690091542" />
+            </motion.div>
+          ))}
         </div>
       </div>
     </>
