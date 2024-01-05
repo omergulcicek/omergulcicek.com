2acf6c9ae6a08c4b28fe424ed97cddedd0adfe93
Ömer Gülçiçek <iletisim@omergulcicek.com>
2024-01-06 00:22:23 +0300

    refactor: color and font size edited

--- src/components/subtitle.jsx
+++ src/components/subtitle.jsx
@@ -5,7 +5,7 @@ export default function Subtitle({ className, children }) {
     <>
       <div
         className={cn(
-          "text-2xl font-bold text-neutral-600  dark:text-neutral-100 mb-4 mt-11",
+          "text-2xl font-bold text-neutral-900  dark:text-neutral-100 mb-4 mt-11",
           className
         )}
       >
--- src/components/title.jsx
+++ src/components/title.jsx
@@ -5,7 +5,7 @@ export default function Title({ className, children }) {
     <>
       <div
         className={cn(
-          "text-3xl md:text-5xl font-bold tracking-wide my-5 text-neutral-600  dark:text-neutral-100 md:mb-8 md:tracking-tight",
+          "text-3xl md:text-5xl font-bold tracking-wide my-5 text-neutral-900  dark:text-neutral-100 md:mb-8 md:tracking-tight",
           className
         )}
       >
