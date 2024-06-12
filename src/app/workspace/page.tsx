25f02779f9a06110ae81182e5636492196114fc4
omergulcicek <iletisim@omergulcicek.com>
2024-06-12 23:39:32 +0300

    Add deskWorkspace data to page component

--- src/app/workspace/page.tsx
+++ src/app/workspace/page.tsx
@@ -3,43 +3,7 @@
 import Container from "@/shared/container";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/ui/tabs";
 
-const deskWorkspace = [
-  {
-    name: "Laptop",
-    image: "https://omergulcicek.com/img/desk/apple-macbook.png",
-    alt: "Apple Macbook Pro M1 13 inch",
-    description: 'Apple Macbook Pro M1, 13"',
-  },
-  {
-    name: "Monitör",
-    image: "https://omergulcicek.com/img/desk/samsung-space.png",
-    alt: "Samsung Space 32' 4K Monitör",
-    description: 'Samsung Space 32" 4K',
-  },
-  {
-    name: "Koltuk",
-    image: "https://omergulcicek.com/img/desk/adore-chair.png",
-    description: "Adore Manager Techno",
-  },
-  {
-    name: "Masa",
-    image: "https://omergulcicek.com/img/desk/nurus-desk.png",
-    alt: "Nurus Otto Basic",
-    description: "Nurus Otto Basic",
-  },
-  {
-    name: "Kablosuz Kulaklık",
-    image: "https://omergulcicek.com/img/desk/logitech-headphone.png",
-    alt: "Logitech G733",
-    description: "Logitech G733",
-  },
-  {
-    name: "Mekanik Klavye",
-    image: "https://omergulcicek.com/img/desk/logitech-keyboard.png",
-    alt: "Logitech G513 Carbon",
-    description: "Logitech G513 Carbon",
-  },
-];
+import { deskWorkspace } from "@/data/deskWorkspace";
 
 export default function Workspace() {
   return (
