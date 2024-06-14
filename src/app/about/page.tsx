675ab736c6b2c32667bd2f8600ca87a723144583
omergulcicek <iletisim@omergulcicek.com>
2024-06-14 21:16:04 +0300

    Add about page and bookmarks stack page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import Link from "next/link";
 
 import Container from "@/shared/container";
--- src/app/workspace/page.tsx
+++ src/app/workspace/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import Container from "@/shared/container";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/ui/tabs";
 
