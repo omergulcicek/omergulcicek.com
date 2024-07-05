6117ea5f0fda2f6665a82b1ae06b4fe7d5985c94
omergulcicek <iletisim@omergulcicek.com>
2024-07-05 23:53:31 +0300

    Add Roadmap page component

---
+++ src/app/roadmap/page.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+import Container from "@/shared/container";
+import Title from "@/ui/title";
+
+export default function Roadmap() {
+  return (
+    <>
+      <Container className="mt-10 md:mt-24 max-w-2xl">
+        <Title tag="h2">Yol Haritası</Title>
+      </Container>
+
+      <div className="mt-10 md:mt-24">
+        <iframe
+          src="https://roadmap.sh/r/embed?id=65ed002373b4b18a31f6f307"
+          width="100%"
+          height="1000px"
+        ></iframe>
+      </div>
+    </>
+  );
+}
