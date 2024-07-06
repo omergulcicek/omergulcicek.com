fc57d246140b0458f0453495628a568c785cdb2c
omergulcicek <iletisim@omergulcicek.com>
2024-07-06 15:37:25 +0300

    Refactor code to improve performance and readability

---
+++ src/app/bootcamp/page.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+import Container from "@/shared/container";
+import Title from "@/ui/title";
+
+export default function Bootcamp() {
+  return (
+    <>
+      <Container className="mt-10 md:mt-24 max-w-2xl">
+        <Title tag="h2">Bootcamp</Title>
+      </Container>
+    </>
+  );
+}
