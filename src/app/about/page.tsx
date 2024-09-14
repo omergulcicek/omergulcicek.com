baf71b56ec98bbfab04d3107868200ef08e82f4e
Ömer Gülçiçek <omer.gulcicek@alisgidis.com>
2024-09-14 14:37:47 +0300

    Update About page content

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -18,19 +18,37 @@ export default function About() {
 				>
 					@Alışgidiş
 				</Link>
-				'te bir NextJS projesi üzerinde çalışıyorum. <br /> UI/UX, Web Vitals,
-				A11Y Project konularına tutkuluyum.
+				'te bir NextJS projesi üzerinde çalışıyorum. <br /> UI/UX, Web Vitals ve
+				A11Y Project konularına büyük bir tutku ve ilgi duyuyorum.
 			</p>
 
 			<p className="text-lg leading-8 text-neutral-500 my-2">
-				Turkuaz ve Türkçe Doküman projeleri başta olmak üzere çalışmalarımın
-				tamamı açık kaynak. Türkçe kaynak sayısını arttırmak adına yazılım
-				alanında çeviriler yapmaktayım. Düzenli olarak makaleler yazıyorum.
+				Projeler geliştirirken yaratıcılığın sınırlarını zorlamak ve yenilikçi
+				çözümler üretmek bana büyük bir tatmin sağlıyor. Çalışmalarımda sadelik
+				ve ölçeklenebilirliğe odaklanarak, küçük detaylarla güçlü duygusal
+				bağlar kurmayı hedefliyorum. Uzun vadeli düşünme ve kullanıcı deneyimini
+				derinlemesine anlamanın önemini vurguluyorum.
 			</p>
 
 			<p className="text-lg leading-8 text-neutral-500 my-2">
-				Bu site için tek motivasyonum, öğrendiklerimi paylaşmak ve belki birkaç
-				kişiyle bağlantı kurmak.{" "}
+				Turkuaz ve Türkçe Doküman projeleri, açık kaynak çalışmalarımda
+				öncelikli projelerim arasında yer alıyor ve tüm projelerimi açık kaynak
+				olarak paylaşıyorum. Ayrıca, yazılım alanındaki bilgi birikimimi
+				artırmak amacıyla düzenli olarak çeviriler yapıyor ve makaleler
+				yazıyorum.
+			</p>
+
+			<p className="text-lg leading-8 text-neutral-500 my-2">
+				Yazılım dışında beni en çok mutlu eden şeyler satranç oynamak,
+				arkadaşlarımla vakit geçirmek ve yeni yerler keşfetmek. Tarih, siyaset
+				ve ekonomi alanlarına ilgim var ve bu konularda sürekli olarak kendimi
+				geliştirmeye çalışıyorum.
+			</p>
+
+			<p className="text-lg leading-8 text-neutral-500 my-2">
+				Bu siteyi, öğrendiklerimi paylaşmak, yazılım topluluğuna katkıda
+				bulunmak ve başkalarıyla bağlantı kurmak amacıyla oluşturuyorum. <br />{" "}
+				Bana{" "}
 				<Link
 					href="mailto:iletisim@omergulcicek.com"
 					rel="noopener noreferrer"
@@ -39,7 +57,7 @@ export default function About() {
 				>
 					iletisim@omergulcicek.com
 				</Link>{" "}
-				adresinden bana ulaşabilirsin.
+				adresinden her zaman ulaşabilirsin.
 			</p>
 		</Container>
 	)
