6ce3d940801d5ca7ce11b137cf061751a942a2e8
Ömer Gülçiçek <omer.gulcicek@alisgidis.com>
2024-10-26 15:20:14 +0300

    Refactor book page and book list components

--- src/components/widgets/book-list.tsx
+++ src/components/widgets/book-list.tsx
@@ -1,15 +1,62 @@
-import { AnimationListLayout } from "@/layouts/animation-list-layout"
+"use client"
 
+import { useEffect, useState } from "react"
+
+import { AnimationListLayout } from "@/layouts/animation-list-layout"
+import { motion } from "framer-motion"
+
+import { Input } from "@/ui/input"
 import { bookData } from "@/data/book-data"
 
 export function BookList() {
-	if (!bookData) return null
+	const [text, setText] = useState("")
+	const [filterData, setFilterData] = useState(bookData)
+	const [mounted, setMounted] = useState(false)
+
+	useEffect(() => {
+		setMounted(true)
+		return () => setMounted(false)
+	}, [])
+
+	useEffect(() => {
+		if (!text) {
+			setFilterData(bookData)
+			return
+		}
+
+		const lowerText = text.toLowerCase()
+		const filteredData = bookData.filter(
+			(data) =>
+				(data.description ?? "").toLowerCase().includes(lowerText) ||
+				(data.title ?? "").toLowerCase().includes(lowerText)
+		)
+
+		setFilterData(filteredData)
+	}, [text])
+
+	if (!filterData || !mounted) return null
 
 	return (
-		<AnimationListLayout
-			data={bookData.sort((a, b) =>
-				(a.description ?? "").localeCompare(b.description ?? "")
-			)}
-		/>
+		<>
+			<motion.article
+				initial={{ opacity: 0, translateY: "100px" }}
+				animate={{ opacity: 1, translateY: "0px" }}
+				transition={{ delay: 0.25 }}
+			>
+				<div className="py-2 w-full md:w-1/2 lg:w-72">
+					<Input
+						type="text"
+						placeholder="Kitap yada yazar ara"
+						onChange={(e) => setText(e.target.value)}
+					/>
+				</div>
+			</motion.article>
+
+			<AnimationListLayout
+				data={filterData.sort((a, b) =>
+					(a.description ?? "").localeCompare(b.description ?? "")
+				)}
+			/>
+		</>
 	)
 }
