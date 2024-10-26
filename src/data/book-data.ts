59168eaa77953228808aa2be1db48a14e904cf3d
Ömer Gülçiçek <omer.gulcicek@alisgidis.com>
2024-10-26 14:26:50 +0300

    Update book URLs in book-data.ts

--- src/data/book-data.ts
+++ src/data/book-data.ts
@@ -5,102 +5,102 @@ export const bookData: AnimationListTypes = [
 		img: "https://r2.1k-cdn.com/sig/size:96/plain/https%3A%2F%2F1k-cdn.com%2Fresimler%2Fkitaplar%2F56586_1705478114_1Ggrf.jpg",
 		title: "İnsanlığın Dirilişi",
 		description: "Sezai Karakoç",
-		url: "https://1000kitap.com/kitap/insanligin-dirilisi--25476"
+		url: "https://www.kitapyurdu.com/kitap/insanligin-dirilisi/6385.html"
 	},
 	{
 		img: "https://r2.1k-cdn.com/sig/size:96/plain/https%3A%2F%2F1k-cdn.com%2Fresimler%2Fkitaplar%2F56586_1680949378_pggrV.jpg",
 		title: "İslamın Dirilişi",
 		description: "Sezai Karakoç",
-		url: "https://1000kitap.com/kitap/islamin-dirilisi--40317"
+		url: "https://www.kitapyurdu.com/kitap/islamin-dirilisi/6514.html"
 	},
 	{
 		img: "https://r2.1k-cdn.com/sig/size:96/plain/https%3A%2F%2F1k-cdn.com%2Fresimler%2Fkitaplar%2F228432_79d7a_1560463657.jpg",
 		title: "Japonizm",
 		description: "Erin Niimi Longhurst",
-		url: "https://1000kitap.com/kitap/japonizm--166856"
+		url: "https://www.kitapyurdu.com/kitap/japonizm-ciltli/499470.html"
 	},
 	{
 		img: "https://r2.1k-cdn.com/sig/size:96/plain/https%3A%2F%2F1k-cdn.com%2Fresimler%2Fkitaplar%2F568685_91c77_1579521590.jpg",
 		title: "Fabrika Ayarı",
 		description: "Hayati İnanç",
-		url: "https://1000kitap.com/kitap/fabrika-ayari--197217"
+		url: "https://www.kitapyurdu.com/kitap/fabrika-ayari/528784.html"
 	},
 	{
 		img: "https://r2.1k-cdn.com/sig/size:96/plain/https%3A%2F%2F1k-cdn.com%2Fresimler%2Fkitaplar%2F226008_6febf_1536179106.jpg",
 		title: "59 Yüz Portreler",
 		description: "Necmettin Asma",
-		url: "https://1000kitap.com/kitap/59-yuz-portreler--121779"
+		url: "https://www.kitapyurdu.com/kitap/59-yuz-portreler/457862.html&filter_name=59+yuz++portreler"
 	},
 	{
 		img: "https://r2.1k-cdn.com/sig/size:96/plain/https%3A%2F%2F1k-cdn.com%2Fresimler%2Fkitaplar%2F23496_WGZ1V_1489160685.jpg",
-		title: "Walter Isaacson",
+		title: "Geleceği Keşfedenler",
 		description: "Walter Isaacson",
-		url: "https://1000kitap.com/kitap/gelecegi-kesfedenler-dijital-cagin-biyografisi--75063"
+		url: "https://www.kitapyurdu.com/kitap/gelecegi-kesfedenler-dijital-cagin-biyografisi/416958.html"
 	},
 	{
 		img: "https://r2.1k-cdn.com/sig/size:96/plain/https%3A%2F%2F1k-cdn.com%2Fresimler%2F%2Fkitaplar%2F55625_1455360274.jpg",
 		title: "Hemdem",
 		description: "Sait Köşk",
-		url: "https://1000kitap.com/kitap/hemdem--55625"
+		url: "https://www.kitapyurdu.com/kitap/hemdem-kalpten-kalbe-bir-yol-vardir/677801.html"
 	},
 	{
 		img: "https://r2.1k-cdn.com/sig/size:96/plain/https%3A%2F%2F1k-cdn.com%2Fresimler%2Fkitaplar%2F39535_YaeS5_1526727883.jpg",
 		title: "Sadece Aptallar 8 Saat Uyur",
 		description: "Erdal Demirkıran",
-		url: "https://1000kitap.com/kitap/sadece-aptallar-8-saat-uyur--2058"
+		url: "https://www.kitapyurdu.com/kitap/sadece-aptallar-8-saat-uyur/81650.html"
 	},
 	{
 		img: "https://r2.1k-cdn.com/sig/size:96/plain/https%3A%2F%2F1k-cdn.com%2Fresimler%2Fkitaplar%2F56586_0f546_1617596755.jpg",
 		title: "Kuşatma - 1453",
 		description: "Okay Tiryakioğlu",
-		url: "https://1000kitap.com/kitap/kusatma-1453--1782"
+		url: "https://www.kitapyurdu.com/kitap/kusatma-1453/130135.html"
 	},
 	{
 		img: "https://r2.1k-cdn.com/sig/size:96/plain/https%3A%2F%2F1k-cdn.com%2Fresimler%2Fkitaplar%2FAllah-De-Otesini-Birak_14363_1385410523.jpg",
 		title: "Allah De Ötesini Bırak",
 		description: "Uğur Koşar",
-		url: "https://1000kitap.com/kitap/allah-de-otesini-birak--14363"
+		url: "https://www.kitapyurdu.com/kitap/allah-de-otesini-birak/311263.html"
 	},
 	{
 		img: "https://r2.1k-cdn.com/sig/size:96/plain/https%3A%2F%2F1k-cdn.com%2Fresimler%2Fkitaplar%2F9414_faec8_1598188535.jpg",
 		title: "Od",
 		description: "İskender Pala",
-		url: "https://1000kitap.com/kitap/od--246"
+		url: "https://www.kitapyurdu.com/kitap/od-/261783.html"
 	},
 	{
 		img: "https://r2.1k-cdn.com/sig/size:96/plain/https%3A%2F%2F1k-cdn.com%2Fresimler%2Fkitaplar%2F27488_1420466595.jpg",
 		title: "Piyon",
 		description: "Cemre Demirel",
-		url: "https://1000kitap.com/kitap/piyon--27488"
+		url: "https://www.kitapyurdu.com/kitap/piyon/321683.html"
 	},
 	{
 		img: "https://r2.1k-cdn.com/sig/size:96/plain/https%3A%2F%2F1k-cdn.com%2Fresimler%2Fkitaplar%2F70673_SUD2c_1523115932.jpg",
 		title: "Kesintisiz Öğrenme",
 		description: "Mümin Sekman",
-		url: "https://1000kitap.com/kitap/kesintisiz-ogrenme--1484"
+		url: "https://www.kitapyurdu.com/kitap/kesintisiz-ogrenme/42711.html"
 	},
 	{
 		img: "https://r2.1k-cdn.com/sig/size:96/plain/https%3A%2F%2F1k-cdn.com%2Fresimler%2Fkitaplar%2F8019_Tennure_ve_Ates-Sinan_Yagmur866.jpg",
 		title: "Tennure ve Ateş",
 		description: "Sinan Yağmur",
-		url: "https://1000kitap.com/kitap/tennure-ve-ates--8019"
+		url: "https://www.kitapyurdu.com/kitap/tennure-ve-ates-hz-mevlana-karton-kapak/240852.html"
 	},
 	{
 		img: "https://r2.1k-cdn.com/sig/size:96/plain/https%3A%2F%2F1k-cdn.com%2Fresimler%2Fkitaplar%2F262977_30982_1535820499.jpg",
-		title: "Aşkın Gözyaşları - Tebrizli Şems",
+		title: "Aşkın Gözyaşları - Şems-i Tebrizi",
 		description: "Sinan Yağmur",
-		url: "https://1000kitap.com/kitap/askin-gozyaslari--121112"
+		url: "https://www.kitapyurdu.com/kitap/askin-gozyaslari-1-sems-tebrizi/408363.html"
 	},
 	{
 		img: "https://r2.1k-cdn.com/sig/size:96/plain/https%3A%2F%2F1k-cdn.com%2Fresimler%2Fkitaplar%2F262977_3c11a_1535820199.jpg",
-		title: "Aşkın Gözyaşları 2 - Hz. Mevlana",
+		title: "Aşkın Gözyaşları 2 - Hz. Mevlânâ",
 		description: "Sinan Yağmur",
-		url: "https://1000kitap.com/kitap/askin-gozyaslari-2--121111"
+		url: "https://www.kitapyurdu.com/kitap/askin-gozyaslari-2-hz-mevlana/408175.html"
 	},
 	{
 		img: "https://r2.1k-cdn.com/sig/size:96/plain/https%3A%2F%2F1k-cdn.com%2Fresimler%2Fkitaplar%2F310398_da59b_1537396314.jpg",
 		title: "Aşkın Gözyaşları 4 - Hamuş",
 		description: "Sinan Yağmur",
-		url: "https://1000kitap.com/kitap/askin-gozyaslari-4-hamus--123897"
+		url: "https://www.kitapyurdu.com/kitap/askin-gozyaslari-4-hamus-olumu-open-dervis/305588.html"
 	}
 ]
