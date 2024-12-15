fa85dec2db03059496b879d552e022916a862d02
Ömer Gülçiçek <omer.gulcicek@alisgidis.com>
2024-12-15 20:26:28 +0300

    Refactor page component to use Roadmap widget

--- src/app/(education)/roadmap/page.tsx
+++ src/app/(education)/roadmap/page.tsx
@@ -1,23 +1,9 @@
-import Link from "next/link"
+import { Roadmap } from "@/widgets/roadmap"
 
-import Container from "@/shared/container"
-import Title from "@/ui/title"
-
-export default function Roadmap() {
+export default function RoadmapPage() {
 	return (
 		<>
-			<Container className="mt-10 md:mt-24 max-w-2xl">
-				<Title tag="h1">Yol Haritası</Title>
-				<Title tag="h2">Frontend yol haritası</Title>
-			</Container>
-
-			<div className="mt-10 md:mt-24">
-				<iframe
-					src="https://roadmap.sh/r/embed?id=65ed002373b4b18a31f6f307"
-					width="100%"
-					height="1000px"
-				></iframe>
-			</div>
+			<Roadmap />
 		</>
 	)
 }
---
+++ src/components/widgets/roadmap.tsx
@@ -0,0 +1,46 @@
+"use client"
+
+import Link from "next/link"
+
+import { motion } from "framer-motion"
+
+import Container from "@/shared/container"
+import Title from "@/ui/title"
+
+export function Roadmap() {
+	return (
+		<>
+			<Container className="flex flex-col gap-6 px-4 md:px-0 mt-10 md:mt-24 max-w-3xl">
+				<motion.h1
+					initial={{ opacity: 0, translateY: "100px" }}
+					animate={{ opacity: 1, translateY: "0px" }}
+					transition={{ delay: 0.05 }}
+				>
+					<Title tag="h1">Yol Haritası</Title>
+				</motion.h1>
+
+				<motion.h2
+					initial={{ opacity: 0, translateY: "100px" }}
+					animate={{ opacity: 1, translateY: "0px" }}
+					transition={{ delay: 0.15 }}
+				>
+					<Title tag="h2">Frontend yol haritası</Title>
+				</motion.h2>
+			</Container>
+
+			<motion.section
+				initial={{ opacity: 0, filter: "blur(64px)" }}
+				animate={{ opacity: 1, filter: "blur(0)" }}
+				transition={{ delay: 0.15, duration: 0.5 }}
+			>
+				<div className="mt-10 md:mt-24">
+					<iframe
+						src="https://roadmap.sh/r/embed?id=65ed002373b4b18a31f6f307"
+						width="100%"
+						height="1000px"
+					></iframe>
+				</div>
+			</motion.section>
+		</>
+	)
+}
