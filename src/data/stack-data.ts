49c8f27bbf4f851dd6ca5d5910ef7e5829f05ddb
Ömer Gülçiçek <omer.gulcicek@alisgidis.com>
2024-12-15 19:30:16 +0300

    Refactor stack data and update technology stack

--- src/data/stack-data.ts
+++ src/data/stack-data.ts
@@ -12,20 +12,20 @@ export const stackData = [
 		img: "https://omergulcicek.com/svg/skills/sass.svg"
 	},
 	{
+		name: "Tailwind CSS",
+		img: "https://omergulcicek.com/svg/skills/tailwindcss.svg"
+	},
+	{
+		name: "shadcn-ui",
+		img: "https://avatars.githubusercontent.com/u/139895814?s=200"
+	},
+	{
 		name: "Styled Components",
 		img: "https://omergulcicek.com/svg/skills/styledcomponents.svg"
 	},
 	{
 		name: "CSS Modules",
 		img: "https://omergulcicek.com/svg/skills/cssmodules.svg"
-	},
-	{
-		name: "BEM",
-		img: "https://omergulcicek.com/svg/skills/bem.svg"
-	},
-	{
-		name: "Tailwind CSS",
-		img: "https://omergulcicek.com/svg/skills/tailwindcss.svg"
 	},
 	{
 		name: "JavaScript",
@@ -36,12 +36,24 @@ export const stackData = [
 		img: "https://omergulcicek.com/svg/skills/react.svg"
 	},
 	{
-		name: "Redux",
-		img: "https://omergulcicek.com/svg/skills/redux.svg"
-	},
-	{
 		name: "Next.js",
 		img: "https://omergulcicek.com/svg/skills/nextjs.svg"
+	},
+	{
+		name: "Zustand",
+		img: "https://user-images.githubusercontent.com/958486/218346783-72be5ae3-b953-4dd7-b239-788a882fdad6.svg"
+	},
+	{
+		name: "Reack Hook Form",
+		img: "https://react-hook-form.com/images/logo/react-hook-form-logo-only.png"
+	},
+	{
+		name: "Zod",
+		img: "https://zod.dev/logo.svg"
+	},
+	{
+		name: "Framer Motion",
+		img: "https://framerusercontent.com/images/s8byoJvwTkn7Gm2jKWNdTPv9F8Q.svg"
 	},
 	{
 		name: "GraphQL",
