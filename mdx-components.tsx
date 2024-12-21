1fd054a37466cc0e5b2fda2ed28710fb8ef70e15
Ömer Gülçiçek <omer.gulcicek@alisgidis.com>
2024-12-21 21:35:28 +0300

    Add lazy loading and full-width styling to img component

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -7,6 +7,7 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
 		h1: ({ children }) => <Title>{children}</Title>,
 		h2: ({ children }) => <Title tag="h2">{children}</Title>,
 		h3: ({ children }) => <Title tag="h3">{children}</Title>,
+		img: (props) => <img {...props} loading="lazy" className="w-full" />,
 		...components
 	}
 }
