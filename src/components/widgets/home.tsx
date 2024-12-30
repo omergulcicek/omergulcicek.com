36b296ffe13f27e7b737986697b44421632bcad7
Ömer Gülçiçek <omer.gulcicek@alisgidis.com>
2024-12-30 20:53:37 +0300

    Refactor home widget: Update layout and remove unnecessary code

--- src/components/widgets/home.tsx
+++ src/components/widgets/home.tsx
@@ -1,35 +1,13 @@
 "use client"
 
-import { cn } from "@/utils"
-import { motion } from "framer-motion"
-
 import { Container } from "@/shared/container"
 import Hero from "@/widgets/hero"
 import { TechStack } from "@/widgets/tech-stack"
-import AnimatedShinyText from "@/ui/animated-shiny-text"
 
 export function Home() {
 	return (
 		<Container className="gap-16 md:gap-20 mt-24 md:mt-56">
-			<section className="flex flex-col gap-2 md:gap-6">
-				<div className="flex items-start">
-					<motion.div
-						initial={{ opacity: 0, filter: "blur(16px)" }}
-						animate={{ opacity: 1, filter: "blur(0)" }}
-						transition={{ delay: 1.75 }}
-					>
-						<div
-							className={cn(
-								"group rounded-full border border-black/5 bg-yellow-100 text-xs md:text-base text-white transition-all ease-in select-none hover:cursor-default dark:border-white/5 dark:bg-yellow-900"
-							)}
-						>
-							<AnimatedShinyText className="inline-flex items-center justify-center px-4 py-1 transition ease-out hover:text-yellow-600 hover:duration-300 hover:dark:text-yellow-400">
-								<span>🚧 Yapım aşamasında...</span>
-							</AnimatedShinyText>
-						</div>
-					</motion.div>
-				</div>
-
+			<section className="flex flex-col gap-2 md:gap-6 md:px-6 xl:px-0">
 				<Hero>
 					<div>
 						<Hero.Hello />
