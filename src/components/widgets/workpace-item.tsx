0afc21e5eab5ff44e075a1e6c7a5ac043584eb0c
Ömer Gülçiçek <omer.gulcicek@alisgidis.com>
2025-01-01 20:41:09 +0300

    Refactor workspace item component: Update layout and remove unnecessary code

--- src/components/widgets/workpace-item.tsx
+++ src/components/widgets/workpace-item.tsx
@@ -1,23 +1,21 @@
-import Link from "next/link"
-
 export const WorkpaceItem = ({ data }: any) => {
 	return (
 		<>
-			<div className="grid grid-cols-1 gap-y-4">
-				{data.map(({ title, subTitle, description, image }: any) => (
-					<div className="grid grid-cols-4 gap-5 shadow-sm border rounded-md p-8">
-						<div className="col-span-3 flex flex-col gap-3">
-							<div className="flex flex-col items-start">
-								<Link href="/" className="font-medium text-blue-400">
-									{title}
-								</Link>
-								<span>{subTitle}</span>
-							</div>
-							<p className="text-secondary-foreground">{description}</p>
-						</div>
-						<figure className="col-span-1">
-							<img src={image} alt={title} className="size-32 rounded-md" />
+			<div className="grid grid-cols-2 gap-4">
+				{data.map(({ title, description, image }: any) => (
+					<div className="relative flex flex-col items-center gap-5 shadow-sm border rounded-md p-8">
+						<figure className="size-32 overflow-hidden mx-auto">
+							<img
+								src={image}
+								alt={title}
+								className="absolute size-32 rounded-md object-contain"
+							/>
 						</figure>
+
+						<div className="flex flex-col items-start">
+							<strong className="font-medium">{title}</strong>
+							<span className="text-secondary-foreground">{description}</span>
+						</div>
 					</div>
 				))}
 			</div>
--- src/data/workspace-data.ts
+++ src/data/workspace-data.ts
@@ -1,51 +1,112 @@
 export const backpackWorkspace = [
 	{
-		title: "backpackWorkspace",
-		subTitle: "Yükseklik Ayarlanabilir Masa",
+		title: "Apple MacBook Pro (M4)",
+		description: "Bilgisayar",
 		image:
-			"https://shop.nurus.com/cdn/shop/files/3_23d7791e-ba1a-4ef5-96e1-521c0827b8b2.jpg?v=1725622799&width=2400",
-		description:
-			"Malzeme kalitesiyle birinci sınıf bir masa ama en kötü tarfı tahta ölçüleri küçük. En ve boy 10'ar cm daha uzun olsa tadından yenmezdi."
+			"https://www.apple.com/v/mac/compare/ab/images/overview/compare_macbook_pro_14_spaceblack__br7yn88wc8nm_large_2x.jpg"
+	},
+	{
+		title: "Apple iPhone 16 Pro",
+		description: "Telefon",
+		image:
+			"https://www.apple.com/v/iphone/compare/ah/images/overview/compare_iphone16_pro_black_titanium__ffs0ixp5spym_large_2x.jpg"
+	},
+	{
+		title: "Apple Watch Ultra 2",
+		description: "Akıllı Saat",
+		image:
+			"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/watch-compare-ultra2-202409_GEO_TR?wid=454&hei=512&fmt=jpeg&qlt=90&.v=1725406773926"
 	},
 	{
 		title: "Apple AirPods 4",
-		subTitle: "Kablosuz Kulaklık",
+		description: "Kablosuz Kulaklık",
 		image:
-			"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/airpods-4-anc-select-202409_FV1?wid=976&hei=916&fmt=jpeg&qlt=90&.v=1725502554118",
-		description:
-			"Kablosuz kulaklık konusunda en iyisi. Ses kalitesi ve kullanımı mükemmel."
+			"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/airpods-4-select-202409_FV1?wid=976&hei=916&fmt=jpeg&qlt=90&.v=1724717692997"
 	}
 ]
 
 export const homeWorkspace = [
 	{
-		title: "homeWorkspace",
-		subTitle: "Yükseklik Ayarlanabilir Masa",
+		title: "Philips OLED 4K Ambilight TV",
+		description: "Televizyon",
 		image:
-			"https://shop.nurus.com/cdn/shop/files/3_23d7791e-ba1a-4ef5-96e1-521c0827b8b2.jpg?v=1725622799&width=2400",
-		description:
-			"Malzeme kalitesiyle birinci sınıf bir masa ama en kötü tarfı tahta ölçüleri küçük. En ve boy 10'ar cm daha uzun olsa tadından yenmezdi."
+			"https://images.philips.com/is/image/philipsconsumer/aa6f1d029566405d96bab12500796468?$pnglarge$&wid=1920"
+	},
+	{
+		title: "Philips Hue",
+		description: "Hoparlör",
+		image:
+			"https://www.assets.signify.com/is/image/Signify/7820230P7-Carousel-MainProduct-RTP-01?wid=1500&hei=1125&qlt=82"
+	},
+	{
+		title: "Apple TV 4K",
+		description: "Eğlence Sistemi",
+		image:
+			"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/apple-tv-4k-gallery4-202210?wid=2824&hei=2400&fmt=jpeg&qlt=90&.v=1664502199656"
+	},
+	{
+		title: "Apple HomePod",
+		description: "Hoparlör",
+		image:
+			"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/homepod-select-midnight-202210?wid=940&hei=1112&fmt=png-alpha&.v=1670557210097"
+	},
+	{
+		title: "TP-Link Deco X20",
+		description: "Wi-Fi 6 Mesh Sistem",
+		image: "https://static.tp-link.com/Deco-X20-01_large_1598239674599u.jpg"
 	}
 ]
 
 export const deskWorkspace = [
 	{
-		title: "deskWorkspace",
-		subTitle: "Yükseklik Ayarlanabilir Masa",
+		title: "Apple Studio Display",
+		description: "Monitör",
 		image:
-			"https://shop.nurus.com/cdn/shop/files/3_23d7791e-ba1a-4ef5-96e1-521c0827b8b2.jpg?v=1725622799&width=2400",
-		description:
-			"Malzeme kalitesiyle birinci sınıf bir masa ama en kötü tarfı tahta ölçüleri küçük. En ve boy 10'ar cm daha uzun olsa tadından yenmezdi."
+			"https://www.apple.com/v/studio-display/c/images/overview/hero/static__bntadi3c3hde_large_2x.jpg"
+	},
+	{
+		title: "Xiaomi Mi",
+		description: "Monitörü Işık Çubuğu",
+		image:
+			"https://i01.appmifile.com/v1/MI_18455B3E4DA706226CF7535A58E875F0267/pms_1640939048.78164781!600x600!85.png"
+	},
+	{
+		title: "Logitech G513 Carbon",
+		description: "Klavye",
+		image:
+			"https://resource.logitechg.com/w_692,c_lpad,ar_4:3,q_auto,f_auto,dpr_2.0/d_transparent.gif/content/dam/gaming/en/products/g513/g513-carbon-gallery-1.png?v=1"
+	},
+	{
+		title: "Logitech G703",
+		description: "Fare",
+		image:
+			"https://resource.logitechg.com/w_692,c_lpad,ar_4:3,q_auto,f_auto,dpr_2.0/d_transparent.gif/content/dam/gaming/en/products/g703/g703-gallery-1.png?v=1"
+	},
+	{
+		title: "Nurus Otto Basic",
+		description: "Yükseklik Ayarlanabilir Masa",
+		image:
+			"https://shop.nurus.com/cdn/shop/files/7_8d248573-06d0-489e-9f6c-646c5e243b02.jpg?v=1725622799&width=2400"
+	},
+	{
+		title: "Herman Miller Embody",
+		description: "Koltuk",
+		image:
+			"https://images.hermanmiller.group/m/3fab92fd854ea081/W-HM_4737_100147356_graphite_graphite_black_b.png?trim=auto&trim-sd=1&blend-mode=darken&blend=fafafa&bg=fafafa&auto=format&w=1200&q=68&h=1200"
 	}
 ]
 
 export const photoWorkspace = [
 	{
-		title: "photoWorkspace",
-		subTitle: "Yükseklik Ayarlanabilir Masa",
+		title: "Canon EOS R5",
+		description: "Kamera",
 		image:
-			"https://shop.nurus.com/cdn/shop/files/3_23d7791e-ba1a-4ef5-96e1-521c0827b8b2.jpg?v=1725622799&width=2400",
-		description:
-			"Malzeme kalitesiyle birinci sınıf bir masa ama en kötü tarfı tahta ölçüleri küçük. En ve boy 10'ar cm daha uzun olsa tadından yenmezdi."
+			"https://s7d1.scene7.com/is/image/canon/4147C002_eos-r5-body_primary?fmt=webp-alpha&wid=1600"
+	},
+	{
+		title: "Canon EF 70-300mm",
+		description: "Lens",
+		image:
+			"https://images.versus.io/objects/canon-ef-70-300-f4-5-6-is-ii-usm.front.variety.1576684641045.jpg"
 	}
 ]
