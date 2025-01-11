ae1cffaeaa4732ec3d20aa77996b9962c9f4f657
Ömer Gülçiçek <omer.gulcicek@alisgidis.com>
2025-01-11 23:22:25 +0300

    Refactor font sizes and styles in layout and title components

--- src/components/widgets/blog-detail-header.tsx
+++ src/components/widgets/blog-detail-header.tsx
@@ -2,12 +2,13 @@
 
 import Link from "next/link"
 
-import { formatDate } from "@/utils"
-
 import BlurFade from "@/ui/blur-fade"
 import { Separator } from "@/ui/separator"
 import { Tag } from "@/ui/tag"
 import { Title } from "@/ui/title"
+
+import { formatDate } from "@/utils"
+import { Medium } from "@/utils/get-icon"
 
 export default async function BlogDetailHeader({
 	title,
@@ -40,16 +41,8 @@ export default async function BlogDetailHeader({
 										className="text-base transition !no-underline"
 									>
 										<div className="flex items-center gap-2">
-											<figure className="flex items-center justify-center size-4 transition duration-300 !my-0">
-												<svg
-													xmlns="http://www.w3.org/2000/svg"
-													viewBox="0 0 640 512"
-													height={16}
-													width={16}
-													className="dark:fill-white"
-												>
-													<path d="M180.5 74.3C80.8 74.3 0 155.6 0 256S80.8 437.7 180.5 437.7 361 356.4 361 256 280.2 74.3 180.5 74.3zm288.3 10.6c-49.8 0-90.2 76.6-90.2 171.1s40.4 171.1 90.3 171.1 90.3-76.6 90.3-171.1H559C559 161.5 518.6 84.9 468.8 84.9zm139.5 17.8c-17.5 0-31.7 68.6-31.7 153.3s14.2 153.3 31.7 153.3S640 340.6 640 256C640 171.4 625.8 102.7 608.3 102.7z" />
-												</svg>
+											<figure className="size-4 transition duration-300 !my-0">
+												<Medium />
 											</figure>
 
 											<span className="text-black dark:text-white">
