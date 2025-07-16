ed9afdd9454d9a6f8a5c4b3af70c7dd9727fa0f0
omergulcicek <iletisim@omergulcicek.com>
2025-07-16 17:33:44 +0300

    Refactor Hero component layout: wrap content in a section, increase font sizes for display name and job title, and maintain styling consistency.

--- src/components/widgets/hero/Hero.tsx
+++ src/components/widgets/hero/Hero.tsx
@@ -6,30 +6,32 @@ import { VerifiedIcon } from "@/icons"
 
 export function Hero() {
 	return (
-		<div className="flex items-end gap-4">
-			<figure className="size-40 rounded-full ring-1 ring-border ring-offset-2 ring-offset-background select-none">
-				<Image
-					src={USER.avatar}
-					alt={USER.displayName}
-					width={160}
-					height={160}
-					quality={100}
-					priority
-				/>
-			</figure>
+		<section>
+			<div className="flex items-end gap-4">
+				<figure className="size-40 rounded-full ring-1 ring-border ring-offset-2 ring-offset-background select-none">
+					<Image
+						src={USER.avatar}
+						alt={USER.displayName}
+						width={160}
+						height={160}
+						quality={100}
+						priority
+					/>
+				</figure>
 
-			<div className="flex flex-col gap-0.5">
-				<div className="flex items-center gap-1">
-					<h1 className="text-2xl font-semibold tracking-tight xl:text-3xl">
-						{USER.displayName}
-					</h1>
-					<VerifiedIcon className="size-4 text-blue-500" />
+				<div className="flex flex-col gap-0.5">
+					<div className="flex items-center gap-1">
+						<h1 className="text-4xl font-semibold tracking-tight">
+							{USER.displayName}
+						</h1>
+						<VerifiedIcon className="size-4 text-blue-500" />
+					</div>
+
+					<p className="text-lg text-muted-foreground select-none">
+						{USER.jobTitle}
+					</p>
 				</div>
-
-				<p className="text-base text-muted-foreground select-none">
-					{USER.jobTitle}
-				</p>
 			</div>
-		</div>
+		</section>
 	)
 }
