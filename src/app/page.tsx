83ca7a6cc3ef0180e5c7227f79d7b1c8ccd86e68
omergulcicek <iletisim@omergulcicek.com>
2025-07-17 15:03:44 +0300

    Add Stack component: introduce a new Stack component wrapped in Section for improved layout; update Home page to include Stack section and adjust widget exports accordingly.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import { About, Blog, Experience, Hero, Projects } from "@/widgets"
+import { About, Blog, Experience, Hero, Projects, Stack } from "@/widgets"
 
 export default function Home() {
 	return (
 		<div className="px-20 py-40 gap-48 flex flex-col mx-auto max-w-3xl">
 			<Hero />
+			<Stack />
 			<Experience />
 			<About />
 			<Blog />
---
+++ src/components/widgets/stack/Stack.tsx
@@ -0,0 +1,9 @@
+import { Section } from "@/widgets"
+
+export function Stack() {
+	return (
+		<Section title="Stack">
+			<div>Stack</div>
+		</Section>
+	)
+}
