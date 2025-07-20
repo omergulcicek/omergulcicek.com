1d292152b83df5a388ddb11661eef0edfefe4206
omergulcicek <iletisim@omergulcicek.com>
2025-07-20 20:24:48 +0300

    Update Footer component and site constants: replace image links with text links in Footer for improved accessibility; add repository URL to site constants for better project visibility.

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -1,27 +1,32 @@
-import Image from "next/image"
-import Link from "next/link"
+import Balancer from "react-wrap-balancer"
+
+import { SITE } from "@/constants/site"
+import { USER } from "@/constants/user"
 
 import { Container } from "@/shared"
+import { LinkPreview } from "@/ui"
 
 export function Footer() {
 	return (
 		<header className="border-t bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 min-h-56">
-			<Container className="py-4">
-				<div className="flex items-center justify-between">
-					<Link href="/">
-						<Image
-							src="/omergulcicek.png"
-							alt="Ömer Gülçiçek"
-							width={32}
-							height={32}
-						/>
-					</Link>
-
-					<nav className="flex flex-col items-end gap-2 mt-4 text-sm font-medium">
-						<Link href="/blog">Blog</Link>
-						<Link href="/projects">Projeler</Link>
-						<Link href="/about">Hakkımda</Link>
-					</nav>
+			<Container>
+				<div className="flex items-center justify-center">
+					<Balancer className="text-muted-foreground text-sm">
+						<LinkPreview
+							url={USER.socials.github}
+							className="text-muted-foreground underline underline-offset-4 font-medium"
+						>
+							{USER.username}
+						</LinkPreview>{" "}
+						tarafından geliştirildi. Kaynak kod{" "}
+						<LinkPreview
+							url={SITE.repository}
+							className="text-muted-foreground underline underline-offset-4 font-medium"
+						>
+							GitHub
+						</LinkPreview>
+						&apos;da mevcuttur.
+					</Balancer>
 				</div>
 			</Container>
 		</header>
--- src/constants/site.ts
+++ src/constants/site.ts
@@ -14,5 +14,6 @@ export const SITE = {
 		"zod",
 		"tanstack-query"
 	],
-	url: "https://omergulcicek.com"
+	url: "https://omergulcicek.com",
+	repository: "https://github.com/omergulcicek/omergulcicek.com"
 }
