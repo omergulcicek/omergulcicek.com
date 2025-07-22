f68b838807b09e49e3a0d498d3538fb7ae730ad4
omergulcicek <iletisim@omergulcicek.com>
2025-07-22 16:26:55 +0300

    Enhance Blog component layout: adjust year display with improved styling and positioning; update post link structure for better accessibility; refine date and tag presentation for enhanced readability.

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -26,27 +26,33 @@ export default function Blog() {
 				title="Blog"
 				description="Teknik, fikir ve yorumlar üzerine makalelerden oluşan bir koleksiyon"
 			>
-				<div className="space-y-12">
+				<div className="space-y-12 mt-10 relative">
 					{Object.entries(postsByYear)
 						.reverse()
 						.map(([year, posts]) => (
-							<div key={year} className="space-y-6 flex flex-col gap-1">
-								<span className="text-xl">{year}</span>
+							<div
+								key={year}
+								className="space-y-6 flex flex-col gap-1 items-start relative min-h-20"
+							>
+								<span className="text-2xl absolute font-medium top-5 -left-12 -rotate-90 text-gray-300 select-none">
+									{year}
+								</span>
 								<div className="flex flex-col gap-6 items-start">
 									{posts.map((post) => (
 										<div className={"flex flex-col gap-1"} key={post.slug}>
-											<Link
-												href={`/blog/${post.slug}`}
-												className="group relative overflow-hidden"
-											>
-												<h2 className="text-lg leading-normal font-normal text-gray-900 group-hover:text-black transition-colors duration-200">
+											<h2 className="text-lg leading-normal font-normal text-gray-900 group-hover:text-black transition-colors duration-200">
+												<Link
+													href={`/blog/${post.slug}`}
+													className="group relative overflow-hidden flex items-start"
+												>
 													{post.metadata.title}
-												</h2>
-											</Link>
+												</Link>
+											</h2>
 											<div className="flex items-start gap-2">
-												<span className="text-xs font-medium text-muted-foreground tracking-wide whitespace-nowrap">
+												<span className="text-xs text-italic font-medium leading-[22px] text-muted-foreground tracking-wide whitespace-nowrap">
 													{dateFormat(post.metadata.createdAt)}
 												</span>
+												<span className="text-black/20 leading-[22px]">•</span>
 												<TagsBadge tags={post.metadata.tags as string[]} />
 											</div>
 										</div>
